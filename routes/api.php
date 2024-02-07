6a55303c1246b88168b9967c9daa987864852804
Krill Shatsila <kirill737shatilo@gmail.com>
2024-02-08 01:26:39 +0300

    Add routing and jwt token

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\Filter\IndexController as FilterIndexController;
 use App\Http\Controllers\Api\Pizza\IndexController;
 use App\Http\Controllers\Api\Pizza\ShowController;
+use App\Http\Controllers\Api\User\StoreController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +19,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix'=>'pizza'],function(){
-    Route::post('/',IndexController::class);
-    Route::get('/{pizza}',ShowController::class);
-    Route::get('/filters',FilterIndexController::class);
+Route::group(['prefix' => 'pizza'], function () {
+    Route::post('/', IndexController::class);
+    Route::get('/{pizza}', ShowController::class);
+    Route::get('/filters', FilterIndexController::class);
+});
 
-  
+Route::group([
+
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me']);
+});
+
+Route::group([  'prefix' => 'users'], function () {
+    Route::post('/',StoreController::class);
+    
 });
